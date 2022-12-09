3cf9402035d9ef116479fd8aa233ee54bac13487
Beatrix Devanti <beatrix.devanti.ft21@mail.umy.ac.id>
2022-12-09 15:35:11 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe4_001
+{
+    class Stacks
+    {
+        private string[] Beatrix;
+        private int top;
+        private int max;
+    }
+}
